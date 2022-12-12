19a7291e468852ceccffc1314b46d59c454d35c3
Sujay Paul <sujaypaul531@gmail.com>
2022-12-13 02:09:39 +0530

    rewritten the logic and added mobile responsive

--- index.js
+++ index.js
@@ -1,61 +1,44 @@
-let bill = document.querySelector("#bill-input")
-let billAmount
-let tip = document.querySelector("#tip-slider")
-let tipPercent = document.querySelector("#tip-percent")
-let tipAmount = document.querySelector("#tip-amount-calculated")
-let tipValue
-let totalBill = document.querySelector("#total-bill")
-let splitSlider = document.querySelector("#split-slider")
-let splitPerson = document.querySelector("#split-person")
+const main = document.querySelector("main")
+const billInput = document.querySelector("#bill-input")
+const tipInput = document.querySelector("#tip-slider")
+const tipPercent = document.querySelector("#tip-percent")
+const totalTip = document.querySelector("#tip-amount-calculated")
+const totalBill = document.querySelector("#total-bill")
+const splitSlider = document.querySelector("#split-slider")
+const splitPerson = document.querySelector("#split-person")
+const billEach = document.querySelector("#bill-each-value")
+const tipEach = document.querySelector("#tip-each-value")
+let billAmount = 0
+let tipAmount = 0
+let totalTipCalculated = 0
+let totalBillCalculated = 0
 let splitSliderValue = 1
-let billEach = document.querySelector("#bill-each-value")
-let tipEach = document.querySelector("#tip-each-value")
 
 
-bill.addEventListener('input', () => {
-    billAmount = bill.value
-    billAmount = parseInt(billAmount)
-    updateAmount()
-})
 
-
-tip.addEventListener('input', () => {
-    tipValue = tip.value
-    tipValue = parseInt(tipValue)
-    tipPercent.innerHTML = tipValue + "%"
-    updateAmount()
-
-})
-
-
-splitSlider.addEventListener('input', () => {
-    splitSliderValue = splitSlider.value
-    splitSliderValue = parseInt(splitSliderValue)
-    splitPerson.innerHTML = splitSliderValue + " Person"
-    updateAmount()
-
-})
-
-let updateAmount = () => {
-    if (billAmount && tipValue) {
-        tipAmount.innerHTML = `Rs ${billAmount * tipValue / 100}`
-        tipEach.innerHTML = `Rs ${(billAmount * tipValue / 100)/splitSliderValue}`
-        totalBill.innerHTML = `Rs ${billAmount + (billAmount * tipValue / 100)}`
-        billEach.innerHTML = `Rs ${billAmount / splitSliderValue}`
-
-
-    } else if (billAmount && !splitSliderValue) {
-        tipAmount.innerHTML = "Rs 0.00"
-        tipEach.innerHTML = "Rs 0.00"
-        totalBill.innerHTML = `Rs ${billAmount}`
-        billEach.innerHTML = `Rs ${billAmount}`
-
-    } else if (billAmount && splitSliderValue) {
-        tipAmount.innerHTML = "Rs 0.00"
-        tipEach.innerHTML = "Rs 0.00"
-        totalBill.innerHTML = `Rs ${billAmount}`
-        billEach.innerHTML = `Rs ${billAmount / splitSliderValue}`
+main.addEventListener("input", (e) => {
+    let input = e.target
+    if (input === billInput) {
+        billAmount = parseInt(billInput.value)
+    } else if (input === tipInput) {
+        tipAmount = parseInt(tipInput.value)
     }
- 
+    else if (input === splitSlider) {
+        splitSliderValue = parseInt(splitSlider.value)   
+    }
+
+    updateAmount()
+})
+
+
+function updateAmount() {
+    tipPercent.innerHTML = tipAmount + "%"
+    totalTipCalculated = (billAmount * tipAmount / 100)
+    totalBillCalculated = Math.floor(totalTipCalculated + billAmount)
+    totalTip.innerHTML = `Rs ${totalTipCalculated}`
+    totalBill.innerHTML = `Rs ${totalBillCalculated}`
+    splitPerson.innerHTML = `${splitSliderValue} Person`
+    billEach.innerHTML = `RS ${Math.floor(totalBillCalculated/splitSliderValue)}`
+    tipEach.innerHTML = `RS ${Math.floor(totalTipCalculated/splitSliderValue)}`
 }
 
